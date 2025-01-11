04fba2d574965bc4e01275eaa20b783c237e1c2b
Raj-Satyam1206 <raj.satyam1229@gmail.com>
2025-01-11 12:37:19 +0530

    Refactored CoinGecko API integration by adding API_OPTIONS and adding detailed comments for improved readability and maintainability.

--- src/context/StorageContext.js
+++ src/context/StorageContext.js
@@ -7,6 +7,7 @@ import {
   useState,
 } from "react";
 import { CryptoContext } from "./CryptoContext";
+import { API_OPTIONS } from "../components/constants";
 
 // create context object
 export const StorageContext = createContext({});
@@ -41,13 +42,15 @@ export const StorageProvider = ({ children }) => {
 
   const getSavedData = async (totalCoins = allCoins) => {
     try {
-      const data = await fetch(
+      const response = await fetch(
         `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${totalCoins.join(
           ","
-        )}&order=${sortBy}&sparkline=false&price_change_percentage=1h%2C24h%2C7d`
+        )}&order=${sortBy}&sparkline=false&price_change_percentage=1h%2C24h%2C7d`, API_OPTIONS
       )
-        .then((res) => res.json())
-        .then((json) => json);
+        // .then((res) => res.json())
+        // .then((json) => json);
+
+        const data = await response.json();
 
       // console.log(data);
       setSavedData(data);
@@ -99,3 +102,37 @@ export const StorageProvider = ({ children }) => {
     </StorageContext.Provider>
   );
 };
+
+
+
+/* 
+Local Storage comes with two functions :-
+1. getItem() - get the value of the item.
+2. setItem() - set the value of the item.
+
+
+First, Local Storage is checked if it has the item "coins" :-
+JSON.parse(localStorage.getItem("coins"))
+
+If it doesn't, localStorage is set with an empty array , otherwise a state variable is made which stores the values in the localStorage
+
+
+Q. Why use JSON.parse() or JSON.stringify()
+- To store a value in the Local Storage, we have to use JSON.stringify() bcoz values are in a JSON string format in the localStorage
+- To access those values, we have to convert the JSON string values into a JavaScript object, that's why, use JSON.parse()
+
+Q. How this Save works?
+- We use two functions- 
+  Save coin()  &&  Remove Coin()
+ -Save coin() 
+    - When the save button is clicked, it calls the saveCoin() with coin_id. 
+    - in SaveCoin(), it checks whether the old array contains this coin with the coin_id
+      - If not, then create a new array by adding this new coin with its coin_id (use JSON.stringify()) 
+
+  -RemoveCoin()
+    - take all the old coins(id) into an array and filter all the coins into the coins into a new array EXCEPT the specified coin with the coin_id.
+    - Now, set the localStorage again with JSON.stringify()  
+    
+-   Star button is clicked and the coins are added to the local storage and then the star button remain clicked until the coins are removed from the local storage.    
+*/ 
+
