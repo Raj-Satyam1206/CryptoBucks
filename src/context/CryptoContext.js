f3c18b1ab2d85809bfd65ce43a17b01d321d7d96
Raj-Satyam1206 <raj.satyam1229@gmail.com>
2025-01-11 12:05:30 +0530

    Refactored CoinGecko API integration by removing unused properties and adding detailed comments for improved readability and maintainability

--- src/context/CryptoContext.js
+++ src/context/CryptoContext.js
@@ -1,5 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { createContext, useLayoutEffect, useState } from "react";
+import { API_OPTIONS } from "../components/constants";
               
 // create context object
 export const CryptoContext = createContext({});
@@ -57,28 +58,28 @@ export const CryptoProvider = ({ children }) => {
     // .then((json) => json): This is another Promise chain. It waits for the conversion to JSON to complete and then returns the JSON data. This line effectively resolves the Promise chain and returns the JSON data retrieved from the API.
 
     try {
-      const data = await fetch(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coinSearch}&order=${sortBy}&per_page=${perPage}&page=${page}&sparkline=false&price_change_percentage=1h%2C24h%2C7d`
-        // `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coinSearch}&order=${sortBy}&per_page=${perPage}&page=${page}&sparkline=false&price_change_percentage=1h%2C24h%2C7d` , API_OPTIONS
+      const response = await fetch(
+        // `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coinSearch}&order=${sortBy}&per_page=${perPage}&page=${page}&sparkline=false&price_change_percentage=1h%2C24h%2C7d`
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coinSearch}&order=${sortBy}&per_page=${perPage}&page=${page}&sparkline=false&price_change_percentage=1h%2C24h%2C7d` , API_OPTIONS
       )
-      .then(async (res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        let errorResponse = await res.json();
-        // here we might get the error so it is best to handle it and throw the error
-        // console.log(errorResponse);
-        setError({ ...error, data: errorResponse.error });
-        throw new Error(errorResponse.error);
-      }).then((json) => json);
-
-      // if (!response.ok) {
-      //   const errorResponse = await response.json();
+      // .then(async (res) => {
+      //   if (res.ok) {
+      //     return res.json();
+      //   }
+      //   let errorResponse = await res.json();
+      //   // here we might get the error so it is best to handle it and throw the error
+      //   // console.log(errorResponse);
       //   setError({ ...error, data: errorResponse.error });
       //   throw new Error(errorResponse.error);
-      // }
+      // }).then((json) => json);
 
-      // const data = await response.json();
+      if (!response.ok) {
+        const errorResponse = await response.json();
+        setError({ ...error, data: errorResponse.error });
+        throw new Error(errorResponse.error);
+      }
+
+      const data = await response.json();
       // console.log(data);
       setCryptoData(data);
     } catch (error) {
@@ -89,13 +90,13 @@ export const CryptoProvider = ({ children }) => {
   const getCoinData = async (coinid) => {
     setCoinData();
     try {
-      const data = await fetch(
-        `https://api.coingecko.com/api/v3/coins/${coinid}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=true&sparkline=false`
-        // `https://api.coingecko.com/api/v3/coins/${coinid}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=true&sparkline=false` , API_OPTIONS
+      const response = await fetch(
+        // `https://api.coingecko.com/api/v3/coins/${coinid}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=true&sparkline=false`
+        `https://api.coingecko.com/api/v3/coins/${coinid}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=true&sparkline=false` , API_OPTIONS
       )
-        .then((res) => res.json())
-        .then((json) => json);
-      //   const data = await response.json();
+        // .then((res) => res.json())
+        // .then((json) => json);
+        const data = await response.json();
       // console.log("CoinData", data);
       setCoinData(data);
     } catch (error) {
@@ -105,13 +106,13 @@ export const CryptoProvider = ({ children }) => {
 
   const getSearchResult = async (query) => {
     try {
-      const data = await fetch(
-        `https://api.coingecko.com/api/v3/search?query=${query}`
-        // `https://api.coingecko.com/api/v3/search?query=${query}` , API_OPTIONS
+      const response = await fetch(
+        // `https://api.coingecko.com/api/v3/search?query=${query}`
+        `https://api.coingecko.com/api/v3/search?query=${query}` , API_OPTIONS
       )
-        .then((res) => res.json())
-        .then((json) => json);
-        // const data = await response.json();
+        // .then((res) => res.json())
+        // .then((json) => json);
+        const data = await response.json();
       // console.log(data);
       setSearchData(data.coins);
     } catch (error) {
@@ -155,3 +156,27 @@ export const CryptoProvider = ({ children }) => {
     </CryptoContext.Provider>
   );
 };
+
+
+
+// The value prop is passed to all the components && the components then destruct the object while using useContext() to have access to their required property.
+
+
+
+
+/*
+cryptoData - Main Table coins data
+
+searchData - Search in input box, list of coins
+
+coinSearch - Clicked a specific coin in the list of coins, details shown on the main table (getCryptoData() is called with coinSerch as id)
+
+currency - (getCryptoData() is called with currency as currency) Whenver currency will change, due to useEffect , getCryptoData() will be called, API is called again with currency value set
+
+sortBy -(getCryptoData() is called with order as sortBy) Whenever an option will be selected from the select tag in Filter, by the event object we will access the option selected and set the state variable (sortBy) to the option.
+Whenver sortBy will change, due to useEffect , getCryptoData() will be called, API is called again with order value set  
+
+coinData - When a coin is clicked, the coin_id is fetched through useParams() in CryptoDetails component, by useEffect , getcoinData() is called , which calls the API. 
+*/
+
+
